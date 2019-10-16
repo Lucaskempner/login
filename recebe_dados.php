927e67661d77914329bcfa104b8b043e78caa561
Lucaskempner <50251362+Lucaskempner@users.noreply.github.com>
2019-10-15 21:50:55 -0300

    aula 06 nao finalizada
    falta terminar

--- recebe_dados.php
+++ recebe_dados.php
@@ -3,7 +3,7 @@
 require_once 'configBD.php';
 
 function verificar_entrada($entrada){
-        $saida = htmlespecialchars($entrada);
+        $saida = htmlspecialchars($entrada);
         $saida = stripslashes($saida);
         $saida = trim($saida);
         return $saida;//retorna a saida limpa
@@ -14,10 +14,65 @@ if (isset($_POST['action'])) {
 
     if ($_POST['action'] == 'cadastro') {
         //teste se ação é igual a cadastro
-        echo "\n<p>cadastro</p>"; //pre-formatada
-        echo "\n<pre>";
-        print_r($_POST);
-        echo "\n</pre>";
+        #echo "\n<p>cadastro</p>"; //pre-formatada
+        #echo "\n<pre>";
+        #print_r($_POST);
+        #echo "\n</pre>";
+        //pegando dados do formulario
+        $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
+        $nomeDoUsuario = verificar_entrada($_POST['nomeDoUsuario']);
+        $emailUsuario = verificar_entrada($_POST['emailUsuario']);
+        $senhaDoUsuario = verificar_entrada($_POST['senhaDoUsuario']);
+        $senhaUsuarioConfirmar = verificar_entrada($_POST['senhaUsuarioConfirmar']);
+        $dataCriado = date("Y-m-d"); //data atual no formato banco de dados
+
+        //codificando as senhas
+        $senhaCodificada = sha1($senhaDoUsuario);
+        $senhaConfirmarCod = sha1($senhaUsuarioConfirmar);
+
+        //teste de captura de dados
+        // echo "<p>Nome completo: $nomeCompleto </p>";
+        // echo "<p>Nome de usuário: $nomeDoUsuario </p>";
+        // echo "<p>E-mail: $emailUsuario </p>";
+        // echo "<p>Senha : $senhaCodificada</p>";
+        // echo "<p>Data de criação: $dataCriado</p>";
+        if($senhaCodificada != $senhaConfirmarCod){
+            echo"<p class='text-danger'>Senhas não conferem.</p>";
+            exit();
+        }else{
+            //as senhas conferem , verificar se o usuario já
+            //existe no banco de dados
+            $sql = $connect->prepare("SELECT nomeDoUsuario, emailUsuario FROM usuario WHERE nomeDoUsuario = ? OR emailUsuario = ?");
+            $sql->bind_param("ss", $nomeDoUsuario, $emailUsuario);
+            $sql->execute();
+            $resultado = $sql->get_result();
+            $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+
+            //verificando a existencia do usuario no banco
+            if($linha['nomeDoUsuario'] == $nomeDoUsuario){
+                echo "<p class='text-danger'> Usuario indisponivel </p>";
+
+            }elseif ($linha['emailUsuario'] == $emailUsuario){
+                echo "<p class='text-danger'> E-mail indisponivel </p>";
+            }else{
+                //usuario pode ser cadastrado no banco de dados
+                $sql = $connect->prepare("INSERT into usuario (nomeDoUsuario,nomeCompleto
+                ,emailUsuario, senhaDoUsuario,dataCriado)values(?,?,?,?,?)");
+                $sql->bind_param("sssss", $nomeDoUsuario, $nomeCompleto, $emailUsuario
+            ,$senhaCodificada, $dataCriado);
+            if($sql->execute()){
+                echo "<p class='text-success'>Usuário cadastrado</p>";
+
+            }else{
+                echo "<p class='text-danger'> Usuário não cadastrado</p>";
+                echo "<p class='text-danger'> Algo deu errado</p>";
+            }
+
+            }
+                # code...
+            }
+        
+
     } else if ($_POST['action'] == 'login') {
         //senão, teste se ação e login
         echo "\n<p>login</p>"; //pre-formatada
